087a2c7f3019d5e9a55f9e65bb39fc48df4638fd
NazGi <104028931+nazarenagg@users.noreply.github.com>
2024-11-02 23:14:43 -0300

    Add files via upload

---
+++ tp1/B2.js
@@ -0,0 +1,44 @@
+function cuadraB2 (  x ,  y ,  ancho ,  alto){
+
+  noStroke();
+  
+  fill(Blancos);
+  rect ( x , y , ancho , alto );//blanco
+  
+    fill( Violetas );
+  rect ( x+3 , y , ancho-3 , alto-3 );//violeta
+  
+    fill(Negros);
+  rect ( x+6 , y , ancho-6 , alto-6 );//negro
+  
+    fill( Verdes );
+  rect ( x+9 , y , ancho-9 , alto-9 );//verde
+  
+    fill(Blancos);
+  rect ( x+12 , y , ancho-12 , alto-12 );//blanco
+  
+    fill( Violetas );
+  rect ( x+15 , y , ancho-15 , alto-15 );//violeta
+  
+    fill( Negros );
+  rect ( x+18 , y , ancho-18 , alto-18 );//negro
+  
+      fill( Verdes );
+  rect ( x+21 , y , ancho-21 , alto-21 );//verde
+  
+    fill( Blancos );
+  rect ( x+24 , y , ancho-24 , alto-24 );//blanco
+  
+    fill( Violetas );
+  rect ( x+27 , y , ancho-27 , alto-27 );//violeta
+  
+    fill(Negros);
+  rect ( x+30 , y , ancho-30 , alto-30 );//negro
+  
+    fill( Verdes );
+  rect ( x+33 , y , ancho-33 , alto-33 );//verde
+  
+    fill(Blancos);
+  rect ( x+36 , y , ancho-36 , alto-36 );//blanco
+
+}
---
+++ tp1/N2.js
@@ -0,0 +1,44 @@
+function cuadraN2 (  x ,  y ,  ancho ,  alto){
+
+  noStroke();
+  
+  fill(Negros);
+  rect ( x , y , ancho , alto );//negro
+  
+    fill( Violetas );
+  rect ( x , y , ancho-3 , alto-3 );//violeta
+  
+    fill(Blancos);
+  rect ( x , y , ancho-6 , alto-6 );//blanco
+  
+    fill( Verdes );
+  rect ( x , y , ancho-9 , alto-9 );//verde
+  
+    fill(Negros);
+  rect ( x , y , ancho-12 , alto-12 );//negro
+  
+    fill( Violetas );
+  rect ( x , y , ancho-15 , alto-15 );//violeta
+  
+    fill(Blancos);
+  rect ( x , y , ancho-18 , alto-18 );//blanco
+  
+      fill( Verdes );
+  rect ( x , y , ancho-21 , alto-21 );//verde
+  
+    fill(Negros);
+  rect ( x , y , ancho-24 , alto-24 );//negro
+  
+    fill( Violetas );
+  rect ( x , y , ancho-27 , alto-27 );//violeta
+  
+    fill(Blancos);
+  rect ( x , y , ancho-30 , alto-30 );//blanco
+  
+    fill( Verdes );
+  rect ( x , y , ancho-33 , alto-33 );//verde
+  
+    fill(Negros);
+  rect ( x , y , ancho-36 , alto-36 );//negro
+
+}
